4bdaf83fb6503f03c1e5468284cb96f0b55cdcaa
c0d33py <rootkill007@gmail.com>
2023-08-17 15:28:07 +0500

    WORKING

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,12 @@
+from re import I
+
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import FileUploaderApi, PostView
 
 urlpatterns = [
     path('', PostView.as_view(), name='post_view'),
     path('files/', FileUploaderApi.as_view(), name='file_upload_api'),
+    path('uppy/', TemplateView.as_view(template_name='uppy.html'), name="uppy"),
 ]
