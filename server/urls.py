9834f81106fec5856e26f606c2d052f7754bc3ef
c0d33py <rootkill007@gmail.com>
2023-08-24 06:57:04 +0500

    Update

--- server/urls.py
+++ server/urls.py
@@ -18,10 +18,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django_tus.urls')),
+    path('', TemplateView.as_view(template_name='tus.html')),
 ]
 
 if settings.DEBUG:
