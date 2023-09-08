2244ec6d801a1bc8cefeee6b9c1a31d74b150fa5
c0d33py <rootkill007@gmail.com>
2023-09-08 21:19:03 +0500

    Check

--- core/views.py
+++ core/views.py
@@ -5,11 +5,11 @@ from .forms import UploadedFileForm
 
 
 def home(request):
-    # if request.method == 'POST':
-    #     form = UploadedFileForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         # form.save()
-    #         return JsonResponse({'message': 'success'})
-    # else:
-    #     form = UploadedFileForm()
-    return render(request, 'index.html', {'form': 'form'})
+    if request.method == 'POST':
+        form = UploadedFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'message': 'success'})
+    else:
+        form = UploadedFileForm()
+    return render(request, 'index.html', {'form': form})
